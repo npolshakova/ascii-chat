52babe9584c5373f1e3af1ca37719f4fcafe3465
Aaron Ray <aray.york@gmail.com>
2019-01-26 12:47:05 -0500

    Added gitignore. Moved image_grab.py

--- example_code/image_grab.py
+++ example_code/image_grab.py
@@ -3,7 +3,6 @@ import cv2
 import time
 camera_port = 0
 camera = cv2.VideoCapture(camera_port)
-time.sleep(0.1)  # If you don't wait, the image will be dark
 return_value, image = camera.read()
 cv2.imwrite("opencv.png", image)
 del(camera)  # so that others can use the camera as soon as possible
