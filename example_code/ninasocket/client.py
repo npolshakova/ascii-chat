a23cb0f467b3cef2f4659b9dd1fc282635dd4d6f
npolshakova <nina_polshakova@brown.edu>
2019-01-26 22:36:54 -0500

    nina's experiment

---
+++ example_code/ninasocket/client.py
@@ -0,0 +1,9 @@
+import socket
+ 
+UDP_IP = "127.0.0.1"
+UDP_PORT = 5005
+MESSAGE = bytes("Hello, World!",'utf-8')
+
+sock = socket.socket(socket.AF_INET, # Internet
+                      socket.SOCK_DGRAM) # UDP
+sock.sendto(MESSAGE, (UDP_IP, UDP_PORT))
