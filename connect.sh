781239a00df74b1dfb9a2a37ab2ee1d8d24999d4
npolshakova <nina_polshakova@brown.edu>
2019-01-27 09:33:44 -0500

    doggo

---
+++ connect.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# Arg 1 is the ip you are connecting to
+# Arg 2 is the port
+# Arg 3 is the Webcam Device Number (Try 0 if unsure)
+# Arg 4 is the Microphone Device Number (Try 0 or 1 if unsure)
+
+./server_launch.sh $1 $2 $3 $4
+
+./receiver_launch.sh $1 $2 
+
