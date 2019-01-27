4c4c2132c6b5d4b71f5330e56b3a5516ab93fa09
npolshakova <nina_polshakova@brown.edu>
2019-01-26 21:49:34 -0500

    First attempt at scripts for launching video and sound streaming

---
+++ receiver_launch.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# Arg 1 is the ip
+# Arg 2 is the port
+
+# Launch the ASCII Viewer
+./receiver_client.py $1 $2 &
+
+# Launch the sound listener
+cvlc udp://$1@:$2
---
+++ sender_launch.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# Arg 1 is the host
+# Arg 2 is the port to host on
+# Arg 3 is the Webcam Device Number (Try 0 if unsure)
+# Arg 4 is the Microphone Device Number (Try 0 or 1 if unsure)
+
+# Set up the Video Stream
+./ascii_webcam_renderer.py $3 | ./sender_server.py $2
+
+# Set up the Sound Stream
+FSAMP=16000
+ffmpeg -f alsa -ar $FSAMP -i hw:$4 -ar $FSAMP -ac 1 -f rtp_mpegts rtp://$1:$($2 + 1)
