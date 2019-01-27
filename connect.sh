e4cc8b970a6d9ced662e9c5246a4285d6a9431e5
Aaron Ray <aray.york@gmail.com>
2019-01-27 10:13:10 -0500

    Run the sender_launch script in background

--- connect.sh
+++ connect.sh
@@ -5,7 +5,7 @@
 # Arg 3 is the Webcam Device Number (Try 0 if unsure)
 # Arg 4 is the Microphone Device Number (Try 0 or 1 if unsure)
 
-./sender_launch.sh $1 $2 $3 $4
+./sender_launch.sh $1 $2 $3 $4 &
 
 ./receiver_launch.sh $1 $2 
 
