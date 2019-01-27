a78d51d4429be66975a4bde725cc24265594235e
npolshakova <nina_polshakova@brown.edu>
2019-01-26 22:03:24 -0500

    ahhhh

--- sender_launch.sh
+++ sender_launch.sh
@@ -6,8 +6,8 @@
 # Arg 4 is the Microphone Device Number (Try 0 or 1 if unsure)
 
 # Set up the Video Stream
-./ascii_webcam_renderer.py $3 | ./sender_server.py $2
+./ascii_webcam_renderer.py $3 | ./sender_server.py $2 &
 
 # Set up the Sound Stream
-FSAMP=16000
-ffmpeg -f alsa -ar $FSAMP -i hw:$4 -ar $FSAMP -ac 1 -f rtp_mpegts rtp://$1:$($2 + 1)
+#FSAMP=16000
+#ffmpeg -f alsa -ar $FSAMP -i hw:$4 -ar $FSAMP -ac 1 -f rtp_mpegts rtp://$1:$(($2 + 1))
